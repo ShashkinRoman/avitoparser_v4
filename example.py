63d4edc435dadc4bd1efa465496a8c50874c98e0
Oleg_Kozlov <oleg221094@yandex.ru>
2020-08-12 11:45:18 +0200

    "fix config"

--- example.py
+++ example.py
@@ -1,21 +1,3 @@
-# from stem import Signal
-# from stem.control import Controller
-# import threading
-# from selenium import webdriver
-# # Смена IP тора (9151 и 9150 - это для браузера тор, для сервера будет 9050 9051 соотвественно)
-# controller = Controller.from_port(port=9151)
-# controller.authenticate()
-# # Получение нового адреса
-#
-# controller.signal(Signal.NEWNYM)
-#
-# controller.close()  # отключение от тора
-#
-# options = webdriver.ChromeOptions()
-# options.add_argument('headless')
-# options.add_argument('--proxy-server=socks5://127.0.0.1:9150')
-
-
 import os
 import sys
 from builtins import object
